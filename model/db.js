8d3bc9f6a474c751cac6f11e1e809fde0e7ae6cd
Leo Gomez <leogomez@gmail.com>
2018-07-30 20:34:15 -0400

    Remove Google Cloud SQL bits

--- model/db.js
+++ model/db.js
@@ -1,16 +1,16 @@
 const { Pool } = require('pg');
 
-const config = {
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PASSWORD,
-  database: process.env.SQL_DATABASE
-}
+// const config = {
+//   user: process.env.SQL_USER,
+//   password: process.env.SQL_PASSWORD,
+//   database: process.env.SQL_DATABASE
+// }
 
-if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
-  config.host = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-}
+// if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+//   config.host = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+// }
 
-const pool = Pool(config);
+const pool = Pool();
 
 pool.connect().catch(err => console.error(err));
 
